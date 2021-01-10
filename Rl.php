058397f82017ed097e024e0dc50a87952d71479e
Yuand　家 <62428058+LetterIce@users.noreply.github.com>
2021-01-10 12:03:05 +0700

    Update Rl.php

--- Rl.php
+++ Rl.php
@@ -2,7 +2,7 @@
 
 require_once 'degoo.class.php';
 try{
-    $degoo = new Degoo('421juanda@gmail.com', 'q86pxcjm0egn' );
+    $degoo = new Degoo('4214juanda@gmail.com', 'q86pxcjm0egn' );
     $result = $degoo->register('max');
     print_r($result);
 } catch (Exception $ex) {
