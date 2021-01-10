cec77b96b03f2828932ab482412e76653807de3c
Yuand　家 <62428058+LetterIce@users.noreply.github.com>
2021-01-10 11:53:12 +0700

    Create Rl.php

---
+++ Rl.php
@@ -0,0 +1,8 @@
+require_once 'degoo.class.php';
+try{
+    $degoo = new Degoo('421juanda@gmail.com', 'q86pxcjm0egn' );
+    $result = $degoo->register('max');
+    print_r($result);
+} catch (Exception $ex) {
+    var_dump($ex->getMessage());
+}
